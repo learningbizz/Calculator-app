ac3a6c333dcada9e0cc7a0c17f15ff25ca50d171
LearningBizz <learningbizz@gmail.com>
2021-09-03 16:26:21 +0200

    Added reset function + added np error + fixed design errors

--- myscript.js
+++ myscript.js
@@ -8,37 +8,52 @@ function reset() {
     2. Select Tip gets reseted (back to default)
     3. Tip amount and total reset to 0 -> Automatic?
     */
+
+    //Point 1
+    document.getElementById('F1').value = 0;
+    document.getElementById('F2').value = 0;
+    document.getElementById('F2').style.outline = null;
+    document.getElementById("errornp").innerHTML = "";
+
+
+    //Point 2
+
+
+    //Point 3
+    document.querySelector(".tipPrice").textContent = "$0.00";
+    document.querySelector(".totalPrice").textContent = "$0.00";
+
     
 }
 
 
 function selectTip(button) {
-    //reset outline in Number of People
+    //reset Number of People
     document.getElementById('F2').style.outline = null;
+    document.getElementById("errornp").innerHTML = "";
+
 
     bill = document.getElementById('F1').value;
     np = document.getElementById('F2').value;
     //STOPPED HERE! getting the name of the button that was clicked
     tip = (button.textContent).slice(0,-1);
 
-    if(np == 0)
-        nperror();
+    if(np == 0) {
+        nperror();   
+    }
+    else {
+            
+        let tipAmount = ((bill/100)*tip)/np;
+        let totalAmount = bill/np + tipAmount;
+        tipAmount = Math.trunc(tipAmount*100)/100;
+        totalAmount = Math.round(totalAmount*100)/100;
 
-    console.log(bill);
-    console.log(tip);
-    console.log(np);
-
-    let tipAmount = ((bill/100)*tip)/np;
-    let totalAmount = bill/np + tipAmount;
-    tipAmount = Math.trunc(tipAmount*100)/100;
-    totalAmount = Math.round(totalAmount*100)/100;
-
-    document.querySelector(".tipPrice").textContent = "$" + tipAmount;
-    document.querySelector(".totalPrice").textContent = "$" + totalAmount;
-
+        document.querySelector(".tipPrice").textContent = "$" + tipAmount;
+        document.querySelector(".totalPrice").textContent = "$" + totalAmount;
+    }
 }
 
 function nperror() {
-    document.getElementById('F2').style.outline = "solid #FF1111"
-    //falta aqui poner el error
+    document.getElementById('F2').style.outline = "solid #C8826D"
+    document.getElementById("errornp").innerHTML = "Can't be zero";
 }
