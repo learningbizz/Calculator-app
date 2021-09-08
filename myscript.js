fac91e3c78cfd27d2570e2b8e823340089b376e3
manuel.tomas.navid <manuel.tomas.navid@estudiantat.upc.edu>
2021-09-08 16:44:11 +0200

    Added Custom Button + fixed extreme versions

--- myscript.js
+++ myscript.js
@@ -1,59 +1,91 @@
 
-let bill = 0, tip = np = 0;
+// np = number people
+let bill = 0, buttonTip = np = 0;
+
+const customTip = document.getElementById('custom');
+
+customTip.addEventListener('input', selectCustomTip);
 
 function reset() {
     /*
-    Three things have to happen:
+    Two things have to happen:
     1. Bill and Number of People go back to 0
-    2. Select Tip gets reseted (back to default)
-    3. Tip amount and total reset to 0 -> Automatic?
+    2. Tip amount and total reset to 0 -> Automatic?
     */
 
     //Point 1
     document.getElementById('F1').value = 0;
     document.getElementById('F2').value = 0;
+    document.getElementById('F1').style.outline = null;
     document.getElementById('F2').style.outline = null;
     document.getElementById("errornp").innerHTML = "";
-
-
-    //Point 2
-
+    document.getElementById("errorbill").innerHTML = "";
+    document.getElementById('custom').value = "";
 
     //Point 3
     document.querySelector(".tipPrice").textContent = "$0.00";
     document.querySelector(".totalPrice").textContent = "$0.00";
 
-    
+
 }
 
 
 function selectTip(button) {
-    //reset Number of People
+    //reseting np & bill and text size of area of calculation if it was too large
     document.getElementById('F2').style.outline = null;
     document.getElementById("errornp").innerHTML = "";
+    document.getElementById('F1').style.outline = null;
+    document.getElementById("errorbill").innerHTML = "";
+    document.querySelector(".tipPrice").style.fontSize = "42px";
+    document.querySelector(".totalPrice").style.fontSize = "42px";
 
 
     bill = document.getElementById('F1').value;
     np = document.getElementById('F2').value;
-    //STOPPED HERE! getting the name of the button that was clicked
-    tip = (button.textContent).slice(0,-1);
+    if (button == null)
+        buttonTip = customTip.value;
+    else
+        buttonTip = (button.textContent).slice(0, -1);
 
-    if(np == 0) {
-        nperror();   
+    if (np <= 0 || bill < 0) {
+        error();
     }
     else {
-            
-        let tipAmount = ((bill/100)*tip)/np;
-        let totalAmount = bill/np + tipAmount;
-        tipAmount = Math.trunc(tipAmount*100)/100;
-        totalAmount = Math.round(totalAmount*100)/100;
 
-        document.querySelector(".tipPrice").textContent = "$" + tipAmount;
-        document.querySelector(".totalPrice").textContent = "$" + totalAmount;
+        let tipAmount = ((bill / 100) * buttonTip) / np;
+        let totalAmount = bill / np + tipAmount;
+        tipAmount = Math.trunc(tipAmount * 100) / 100;
+        totalAmount = Math.round(totalAmount * 100) / 100;
+
+
+        if (tipAmount <= 10000) {
+            document.querySelector(".tipPrice").textContent = "$" + tipAmount;
+            document.querySelector(".totalPrice").textContent = "$" + totalAmount;
+        }
+        else {
+            document.querySelector(".tipPrice").style.fontSize = "x-large";
+            document.querySelector(".totalPrice").style.fontSize = "x-large";
+            document.querySelector(".tipPrice").textContent = "You generous person! :D";
+            document.querySelector(".totalPrice").textContent = "Just leave your house keys.";
+        }
     }
 }
 
-function nperror() {
-    document.getElementById('F2').style.outline = "solid #C8826D"
-    document.getElementById("errornp").innerHTML = "Can't be zero";
+function selectCustomTip() {
+    selectTip(null);
+}
+
+function error() {
+    if(np <= 0) {
+        document.getElementById('F2').style.outline = "solid #C8826D"
+        if (np < 0) 
+            document.getElementById("errornp").innerHTML = "Can't be negative!";
+        else
+        document.getElementById("errornp").innerHTML = "Can't be zero";
+    }
+    if (bill < 0) {
+        document.getElementById('F1').style.outline = "solid #C8826D"
+        document.getElementById("errorbill").innerHTML = "Can't be negative!";
+
+    }
 }
